fe06e775efc38a3385fc4fcfb445ca5a260a6b4e
Hm | edihasam <masa.ishi.right@gmail.com>
2019-03-05 22:50:37 +0900

    [create] test for tasks crud

--- spec/requests/tasks_api_spec.rb
+++ spec/requests/tasks_api_spec.rb
@@ -4,32 +4,81 @@ require 'rails_helper'
 
 RSpec.describe 'TasksApi', type: :request do
   describe 'tasks' do
-    it 'show' do
-      FactoryBot.create(:task, title: 'loads a task やで')
+    context 'index GET /api/v1/tasks' do
+      before do
+        FactoryBot.create(:task, title: 'prepare rails api server')
+        FactoryBot.create(:task, title: 'call web api through react')
+        get '/api/v1/tasks'
+      end
 
-      # tasks/index へ access する
-      get api_v1_tasks_path
-      expect(response).to have_http_status(:success)
+      it 'returns status 200' do
+        expect(response).to have_http_status(200)
+      end
 
-      # index で取得した json から show で見たい task を選択
-      json = JSON.parse(response.body)
-      expect(json['data'].length).to eq 1
-      task_id = json['data'][0]['id']
-
-      # show で見られるか検証
-      get api_v1_task_path(task_id)
-      expect(response).to have_http_status(:success)
-      json = JSON.parse(response.body)
-      expect(json['data']['title']).to eq 'loads a task やで'
+      it 'returns tasks list' do
+        json = JSON.parse(response.body)
+        expect(json['data'][0]['title']).to eq('prepare rails api server')
+        expect(json['data'][1]['title']).to eq('call web api through react')
+      end
     end
 
-    it 'create' do
+    context 'show GET /api/v1/tasks/:id' do
+      before do
+        @task = FactoryBot.create(:task, title: 'write some test code')
+        get "/api/v1/tasks/#{@task.id}"
+      end
+
+      it 'returns status 200' do
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'returns a task' do
+        json = JSON.parse(response.body)
+        expect(json['data']['title']).to eq 'write some test code'
+      end
     end
 
-    it 'update' do
+    context 'create POST /api/v1/tasks' do
+      it 'returns status 200' do
+        task_attributes = FactoryBot.attributes_for(:task)
+        post '/api/v1/tasks', params: { task: task_attributes }
+        expect(response).to have_http_status(200)
+      end
+
+      it 'counts up by 1' do
+        task_attributes = FactoryBot.attributes_for(:task)
+        expect {
+          post '/api/v1/tasks', params: { task: task_attributes }
+        }.to change(Task.all, :count).by(1)
+      end
     end
 
-    it 'destroy' do
+    context 'update PATCH /api/v1/tasks/:id' do
+      before do
+        @task = FactoryBot.create(:task)
+        task_attributes = FactoryBot.attributes_for(:task, title: 'updated title')
+        patch "/api/v1/tasks/#{@task.id}", params: { task: task_attributes }
+      end
+
+      it 'returns status 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'changes title' do
+        expect(@task.reload.title).to eq 'updated title'
+      end
+    end
+
+    context 'destroy DELETE /api/v1/tasks/:id' do
+      before do
+        @task = FactoryBot.create(:task)
+      end
+
+      it 'deletes a task' do
+        expect {
+          delete "/api/v1/tasks/#{@task.id}"
+        }.to change(Task.all, :count).by(-1)
+      end
     end
   end
 end
